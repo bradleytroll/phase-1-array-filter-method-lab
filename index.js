0d5296087d3992678c444af5b63b56cd6e63085d
Bradley Troll <bradleytroll@gmail.com>
2023-06-20 12:30:20 -0400

    complete lab:

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
+// Redoing old labs for practice. New code written below with original code commented out beneath.
+
 function findMatching(drivers, string) {
-    return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
-  }
+  return drivers.filter(function(driver) {
+    return driver.toLowerCase() === string.toLowerCase();
+  });
+}
+
+// Rewritten as arrow function: 
+// function findMatching(drivers, string) {
+//   return drivers.filter(driver => driver.toLowerCase()n === string.toLowerCase());
+// }
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function(driver) {
+    return driver.toLowerCase().startsWith(string.toLowerCase());
+  });
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function(driver) {
+    return driver.name === string;
+  });
+}
+
+// function findMatching(drivers, string) {
+//     return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+//   }
  
-  function fuzzyMatch(array, string) {
-    return array.filter((driver) => {
-      const regex = new RegExp(`^${string}`, 'i');
-      return regex.test(driver);
-    });
-  }
-  function matchName(drivers, string) {
-    return drivers.filter(driver => driver.name === string);
-  }
+//   function fuzzyMatch(array, string) {
+//     return array.filter((driver) => {
+//       const regex = new RegExp(`^${string}`, 'i');
+//       return regex.test(driver);
+//     });
+//   }
+//   function matchName(drivers, string) {
+//     return drivers.filter(driver => driver.name === string);
+//   }
